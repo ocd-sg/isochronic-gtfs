097f146fb9d2020fb31f5eee6e295c10c2dc5416
Yin Shanyang <shanyang@box.local>
2018-04-26 02:18:49 +0800

    feature: port fetch/bus

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-const fetchBus$ = require('./utils/fetch/bus')
+const { Observable } = require('rxjs')
+const fetchBus = require('./utils/fetch/bus')
+const formatBus = require('./utils/format/bus')
+const fetchTrain = require('./')
+
+const bus$ = Observable.of(null)
+  .concatMap(fetchBus)
+  .concatMap(formatBus)
+
+const train$ = Observable.of(null)
+  .concatMap(fetchTrain)
+
+const main$ = Observable.forkJoin(
+  // bus$
+)
 
 console.clear()
-fetchBus$.subscribe(console.log.bind(console))
+main$.subscribe()
